7c02233e4db5cbc712c8ca6b2ee25d7b383acca9
Sarfraz Ahmad <sarfrazahmadiit@gmail.com>
2026-09-22 23:13:47 +0530

    Add user profile controller

--- user-backend/src/controllers/authController.js
+++ user-backend/src/controllers/authController.js
@@ -109,7 +109,30 @@ const loginUser = async (req, res) => {
   }
 };
 
+const getProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.userId).select("-password");
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    res.status(200).json({
+      message: "Profile fetched successfully",
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error",
+      error: error.message,
+    });
+  }
+};
+
 module.exports = {
   registerUser,
   loginUser,
+  getProfile, 
 };
