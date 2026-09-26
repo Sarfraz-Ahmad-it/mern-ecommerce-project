e6d4b20c3cb60b3b5b956490f6248e53e33c7e3b
Sarfraz Ahmad <sarfrazahmadiit@gmail.com>
2026-09-26 10:15:43 +0530

    Improve navbar authentication and sticky layout

--- user-frontend/src/components/Navbar.jsx
+++ user-frontend/src/components/Navbar.jsx
@@ -14,7 +14,7 @@ function Navbar() {
   };
 
   return (
-    <nav className="bg-blue-600 text-white px-4 py-3 sm:px-6 lg:px-8">
+    <nav className="sticky top-0 z-50 bg-blue-600 text-white px-4 py-3 sm:px-6 lg:px-8 shadow-lg">
       <div className="max-w-7xl mx-auto flex items-center justify-between">
 
         {/* Logo */}
