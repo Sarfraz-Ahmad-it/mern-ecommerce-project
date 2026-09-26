e6788d34e5705009d4922ce7558d98904a7896d6
Sarfraz Ahmad <sarfrazahmadiit@gmail.com>
2026-09-26 10:15:28 +0530

    Add user profile controller

--- user-backend/src/controllers/authController.js
+++ user-backend/src/controllers/authController.js
@@ -32,8 +32,21 @@ const registerUser = async (req, res) => {
       password: hashedPassword,
     });
 
+    // Generate JWT token
+    const token = jwt.sign(
+      {
+        userId: user._id,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7d",
+      }
+    );
+
+    // Send response
     res.status(201).json({
       message: "User registered successfully",
+      token,
       user: {
         id: user._id,
         name: user.name,
